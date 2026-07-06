568cddb2b8c3bb05f4bc7a16b3f2e79c13b1c80d
sujitgiri17 <sujit.giri321@gmail.com>
2026-07-06 12:35:29 +0530

    Initial commit

---
+++ DsaArraysString/src/com/arrays/FindLargestEle.java
@@ -0,0 +1,415 @@
+package com.arrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+
+public class FindLargestEle {
+	
+	public static void LargestEle(int[] arr) {
+		int max = arr[0];
+		for(int i=1;i<arr.length;i++) {
+			if(arr[i]>max) {
+				max = arr[i];
+			}
+		}
+		System.out.println(max);
+	}
+	
+	public static void SecLargestEle(int[] arr) {
+		int max = arr[0];
+		int secMax = arr[0];
+		
+		for(int i=1;i<arr.length;i++) {
+			if(max<arr[i]) {
+				secMax = max;
+				max = arr[i];
+			}
+			else if(arr[i]>secMax && secMax!=max) {
+				secMax = arr[i];
+			
+			}
+		}
+		System.out.println(secMax);
+	}
+	
+	public static int[] reverseArr(int[] arr2) {
+		int left=0;
+		int right = arr2.length-1;
+		
+		while(left<right) {
+				int temp = arr2[left];
+				arr2[left] = arr2[right];
+				arr2[right] = temp;
+			left++;
+			right--;
+		}
+		return arr2;
+	}
+	
+	public static int minElement(int [] arr3) {
+		int min = arr3[0];
+		for(int nums : arr3) {
+			if(nums<min) {
+				min = nums;
+			}
+		}
+		return min;
+	}
+	
+	public static void evenOrOdd(int [] arr4) {
+		int evenCount = 0;
+		int OddCount = 0;
+		
+		for(int nums : arr4) {
+			if(nums%2==0) {
+				evenCount++;
+			}else {
+				OddCount++;
+			}
+		}
+		System.out.println("Even Count: "+ evenCount);
+		System.out.println("Odd Count: " + OddCount);
+	}
+	
+	public static int eleFoundOrNot(int [] arr2,int target) {
+		for(int i=0;i<arr2.length;i++) {
+			if(arr2[i]==target) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+		public static int Freq(int[] arr5,int target) {
+			int frequency = 0;
+			for(int nums : arr5) {
+				if(nums == target) {
+					frequency++;
+				}
+			}
+			return frequency;
+		}
+		
+		public static int[] moveZero(int[] arr6) {
+			int first=0;
+			for(int i=0;i<arr6.length;i++) {
+				if(arr6[i]!=0) {
+					int temp=arr6[first];
+					arr6[first]=arr6[i];
+					arr6[i]=temp;
+					first++;
+				}
+			}
+			return arr6;
+		}
+		
+		public static int missingEle(int[] arr7) {
+			//int expected = 0;
+			int actual = 0;
+			int n = arr7.length+1;
+//			for(int i=1;i<=n;i++) {
+//				expected+=i;
+//			}
+			int expected = n*(n+1)/2;
+			
+			for(int i=0;i<arr7.length;i++) {
+				actual+=arr7[i];
+			}
+			int missing = expected-actual;
+			return missing;
+		}
+		
+		public static int secSmallEle(int[] arr8) {
+			int firstSmall=Integer.MAX_VALUE;
+			int SecondSmall=Integer.MAX_VALUE;
+			for(int i=0;i<arr8.length;i++) {
+				if(firstSmall>arr8[i]) {
+					SecondSmall = firstSmall;
+					firstSmall =arr8[i];
+				}
+				else if(SecondSmall>arr8[i] && SecondSmall!=firstSmall) {
+					SecondSmall=arr8[i];
+				}
+			}
+			return SecondSmall;
+		}
+		
+		public static int buyAndSell(int[] prices) {
+			int maxprofit = 0;
+			int buy = prices[0];
+			for(int i=1;i<prices.length;i++) {
+				if(buy>prices[i]) {
+					buy= prices[i];
+				}
+				int profit = prices[i]-buy;
+				
+				maxprofit = Math.max(maxprofit, profit);	
+			}
+			return maxprofit;
+		}
+		
+		public static int[] twoSum(int[] arr9 , int target) {
+			for(int i=0;i<arr9.length;i++) {
+				for(int j=i+1;j<arr9.length;j++) {
+					int add = arr9[i]+arr9[j];
+					if(add==target) {
+						return new int[] {i , j};
+					}
+				}
+			}
+			return new int[] {-1,-1};
+		}
+		
+		public static void arrList(int[] arr2) {
+			ArrayList<Integer> list = new ArrayList<>();
+			
+			for(int i=0;i<arr2.length;i++) {
+				list.add(arr2[i]);
+			}
+			
+			if(list.contains(30)) {
+				System.out.println("Element Found");
+			}else {
+				System.out.println("Element Not Found");
+			}
+//			for(int i=0;i<list.size();i++) {
+//				System.out.println(list.get(i));
+//			}
+		}
+		
+		public static void largestEle(int[] arrr) {
+			ArrayList<Integer> list = new ArrayList<>();
+			int max = arrr[0];
+			for(int i=0;i<arrr.length;i++) {
+				 list.add(arrr[i]);
+			}
+			for(int i=0;i<list.size();i++) {
+				if(list.get(i)>max) {
+					max = list.get(i);
+				}
+			}
+			System.out.println(max);
+		}
+		
+		public static int secondLarge(int[] arr3) {
+			int max = arr3[0];
+			int secMax = Integer.MIN_VALUE;
+			ArrayList<Integer> list = new ArrayList<>();
+			for(int i=0;i<arr3.length;i++) {
+				list.add(arr3[i]);
+			}
+			for(int i=0;i<list.size();i++) {
+				if(list.get(i)>max) {
+					 secMax = max;
+					max = list.get(i);
+				}
+				else if(list.get(i)>secMax && secMax!=max) {
+					secMax = list.get(i);
+				}
+			}
+			return secMax;
+		}
+		
+		public static void Mappp(int[] arrmap) {
+			HashMap<Integer,Integer> map = new HashMap<>();
+			
+			for(int i=0;i<arrmap.length;i++) {
+				if(map.containsKey(arrmap[i])) {
+					map.put(arrmap[i], map.get(arrmap[i])+1);
+				}else {
+					map.put(arrmap[i], 1);
+				}
+				
+			}
+			
+			for(Integer key : map.keySet()) {
+				System.out.println(key + " -> " + map.get(key));
+			}
+		}
+		
+		public static void mappp2() {
+			HashMap<Integer , String> map = new HashMap<>();
+			
+			map.put(101, "Sujit");
+			map.put(102, "Rahul");
+			map.put(103, "Amit");
+			
+			System.out.println( map.get(102)); 
+			System.out.println(map.containsKey(104));
+			map.replace(102, "Rohan");
+			System.out.println(map);
+			
+		}
+		
+		public static int freq(int[] arrrr , int target) {
+			HashMap<Integer , Integer> map = new HashMap<>();
+			
+			for(int i=0;i<arrrr.length;i++) {
+					map.put(arrrr[i], map.getOrDefault(arrrr[i], 0) +1);
+			}
+			return map.getOrDefault(target, 0);
+		}
+		
+		
+		
+		public static int distinctElements(int[] dist) {
+		    HashMap<Integer , Integer> map = new HashMap<>();
+		    
+		    for(int num : dist) {
+		    	map.put(num, map.getOrDefault(num,0)+1);
+		    }
+			
+		    return map.size();
+		}
+		
+		public static int firstReptd(int[] firstrep) {
+			HashMap<Integer,Integer> map = new HashMap<>();
+			for(int num : firstrep) {
+				map.put(num, map.getOrDefault(num, 0)+1);
+			}
+			
+			for(int num : firstrep) {
+				if(map.get(num)>1) {
+					return	num;
+				}
+			}
+			return -1;
+		}
+		
+		public static int majEle(int[] leetcode) {
+			int n = leetcode.length;
+			int half = n/2;
+			HashMap<Integer,Integer> map = new HashMap<>();
+			for(int num : leetcode) {
+				map.put(num, map.getOrDefault(num, 0)+1);
+			}
+			for(int num : map.keySet()) {
+				if(map.get(num)>half) {
+					return num;
+				}
+			}
+			return -1;
+			
+		}
+		
+		
+		//HashSet
+		public static void uniqueElements(int[] sst) {
+			HashSet<Integer> set = new HashSet<>();
+			
+			for(int num : sst) {
+				set.add(num);
+			}
+			
+			for(int num : set) {
+				System.out.println(num);
+			}
+			
+		}
+		
+		public static boolean containsDuplicate(int[] dup) {
+			HashSet<Integer> set = new HashSet<>();
+			
+			for(int num : dup ) {
+				if(set.contains(num)) {
+					return true;
+				}
+				set.add(num);
+			}
+			
+			return false;
+		}
+		
+		public static void intersection(int[] arrr1, int[] arrr2) {
+			
+			HashSet<Integer> set =new HashSet<>();
+			
+			for(int num : arrr1) {
+				set.add(num);
+			}
+			for(int num : arrr2) {
+				
+				if( set.contains(num)) {
+				System.out.println(num);
+				set.remove(num);
+				}
+			}
+			
+		}
+
+	public static void main(String[] args) {
+		int[] arr = {12,45,7,89,26,56};
+		int[] arr2 = {10,20,30,40,50};
+		int arr3[] = {12, 45, 7, 89, 26, 56};
+		int arr4[] = {12, 45, 7, 89, 23, 56, 10, 21};
+		int[] arr5 = {10, 20, 10, 30, 10, 40, 20};
+		int arr6[] = {0, 1, 0, 3, 12};
+		int arr7[] = {1, 2, 4, 5, 6};
+		int arr8[] = {12, 5, 7, 1, 20, 3};
+		int prices[] = {7, 1, 5, 3, 6, 4};
+		int arrr[] = {10,45,7,89,26};
+		int[] arrmap = {1, 2, 1, 3, 2, 1};
+		LargestEle(arr);
+		SecLargestEle(arr);
+		int[] array = reverseArr(arr2);
+		System.out.println(Arrays.toString(array)); 
+		System.out.println(minElement(arr3));  
+		evenOrOdd(arr4);
+		
+		int index = eleFoundOrNot(arr2, 30);
+		if(index != -1) {
+			System.out.println("Element found at index " + index);
+		}else {
+			System.out.println("Element not found");
+		}
+		
+		System.out.println(Freq(arr5,10));
+		System.out.println(Arrays.toString(moveZero(arr6)));
+		
+		System.out.println(missingEle(arr7));
+		System.out.println(secSmallEle(arr8));
+		System.out.println(buyAndSell(prices));
+		
+		int arr9[] = {2, 7, 11, 15};
+		System.out.println(Arrays.toString(twoSum(arr9 , 9)));
+		
+		//Collection FreamWork
+		arrList(arr2);
+		largestEle(arrr);
+		
+		Mappp(arrmap);
+		mappp2();
+		
+		
+		int[] arrrr = {10, 20, 10, 30, 10, 40, 20};
+		int target = 10;
+		System.out.println(freq(arrrr , target));
+		
+		int[] dist = {1,2,2,3,4,4,5};
+		System.out.println(distinctElements(dist));
+		
+		int[] firstrep = {10,5,3,4,3,5,6};
+		System.out.println(distinctElements(firstrep));
+		
+		int[] leetcode = {2,2,1,1,1,2,2};
+		System.out.println(majEle(leetcode)); 
+		
+		//HashSet
+		int[] sst = {10,20,10,30,40,20,50};
+		uniqueElements(sst);
+		
+		int[] dup = {10,20,30,40,20};
+		System.out.println(containsDuplicate(dup)); 
+		
+		int[]arrr1 = {1,2,2,3,4};
+
+		int[] arrr2 = {2,2,4,6};
+		intersection(arrr1,arrr2);
+		
+	}
+
+}
---
+++ DsaArraysString/src/com/string/StringProbs.java
@@ -0,0 +1,234 @@
+package com.string;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StringProbs {
+	
+	public static String revs(String str) {
+		String  string= "";
+		for(int i=str.length()-1;i>=0;i--) {
+			string += str.charAt(i);
+		}
+		return string;
+	}
+	
+	public static int vowels(String str2) {
+		int count = 0;
+		for(int i=0;i<str2.length();i++) {
+			char ch = str2.charAt(i);
+			if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u') {
+				count++;
+			}
+		}
+		return count;
+	}
+	
+	public static void palindrome(String str3) {
+		int first = 0;
+		int last = str3.length()-1;
+		
+		while(first<last) {
+			if(str3.charAt(first)!=str3.charAt(last)) {
+				System.out.println("not palindrome");
+				return;
+			}
+			first++;
+			last--;	
+		}
+		System.out.println("palindorme");
+	}
+	
+	public static void UpperOrLower(String str4) {
+		int UpperCase = 0;
+		int lowerCase = 0;
+		for(int i=0;i<str4.length();i++) {
+			if(str4.charAt(i)>='A' && str4.charAt(i)<='Z') {
+				UpperCase++;
+			}else if(str4.charAt(i)>='a' && str4.charAt(i)<='z') {
+				lowerCase++;
+			}
+		}
+		System.out.println("UpperCase: "+UpperCase);
+		System.out.println("LowerCase: "+lowerCase);
+	}
+	
+	public static void duplicate(String str5) {
+		for(int i=0;i<str5.length();i++) {
+			for(int j=i+1;j<str5.length();j++) {
+				if(str5.charAt(i)==str5.charAt(j)) {
+					System.out.println(str5.charAt(i));
+					break;
+				}
+			}
+		}
+	}
+	
+	public static void mixed(String str6) {
+		int letters = 0;
+		int digits = 0;
+		int special = 0;
+		
+		for(int i=0;i<str6.length();i++) {
+			int ch = str6.charAt(i);
+			if((ch>='A' && ch<='Z') || (ch>='a' && ch<='z')) {
+				letters++;
+			}else if(ch>='0' && ch<='9') {
+				digits++;
+			}else {
+				special++;
+			}
+		}
+		System.out.println("Letters: "+letters);
+		System.out.println("digits: "+ digits);
+		System.out.println("special: "+special);
+	}
+	
+	public static boolean isAnagram(String str7 , String str8) {
+		
+		if(str7.length()!=str8.length()) {
+			return false;
+		}
+		for(int i=0;i<str7.length();i++) {
+			int count1=0;
+			int count2=0;
+			
+			for(int j=0;j<str7.length();j++) {
+				if(str7.charAt(i)==str7.charAt(j)) {
+					count1++;
+				}
+				if(str7.charAt(i)==str7.charAt(j)) {
+					count2++;
+				}
+			}
+			if(count1!=count2) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public static void nonReptd(String str5) {
+		for(int i=0;i<str5.length();i++) {
+			int count=0;
+			for(int j=0;j<str5.length();j++) {
+				if(str5.charAt(i)==str5.charAt(j)) {
+					count++;
+				}
+			}
+			if(count==1) {
+				System.out.println(str5.charAt(i));
+				return;
+			}
+		}
+		
+	}
+	
+	public static void removeDupli(String str5) {
+		String result = "";
+		for(int i=0;i<str5.length();i++) {
+			boolean found = false;
+			for(int j=0;j<result.length();j++) {
+				if(str5.charAt(i)==result.charAt(j)) {
+					found = true;
+					break;
+				}
+			}
+			if(!found) {
+				result+=str5.charAt(i);
+			}
+			
+		}
+		System.out.println(result);
+	}
+	
+	public static void freq(String str5) {
+		HashMap<Character , Integer> map = new HashMap<>();
+		
+		for(int i=0;i<str5.length();i++) {
+			
+			map.put(str5.charAt(i), map.getOrDefault(str5.charAt(i), 0)+1);
+			
+		}
+		for(Map.Entry<Character, Integer>entry: map.entrySet()) {
+			System.out.println(entry.getKey()+ " -> "+ entry.getValue());
+		}
+	}
+	
+	public static boolean validAna(String str7 , String str8) {
+		HashMap<Character, Integer> map = new HashMap<>();
+		
+		if(str7.length()!=str8.length()) {
+			return false;
+		}
+		for(char ch : str7.toCharArray()) {
+			map.put(ch, map.getOrDefault(ch, 0)+1);
+		}
+		for(char ch : str8.toCharArray()) {
+			if(!map.containsKey(ch)) {
+				return false;
+			}
+			map.put(ch, map.get(ch)-1);
+			
+			if(map.get(ch)==0) {
+				map.remove(ch);
+			}
+		}
+		return map.isEmpty();
+		
+	}
+	
+	public static int firstUnique(String stre) {
+		HashMap<Character, Integer> map = new HashMap<>();
+		
+		for(int i=0;i<stre.length();i++) {
+			map.put(stre.charAt(i), map.getOrDefault(stre.charAt(i), 0)+1);
+		}
+		
+		for(int i=0;i<stre.length();i++) {
+			if(map.get(stre.charAt(i))==1) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+
+	public static void main(String[] args) {
+		String str = "JAVA";
+		System.out.println(revs(str));
+		
+		String str2 = "education";
+		System.out.println(vowels(str2));
+		
+		String str3 = "madam";
+		palindrome(str3);
+		
+		String str4 = "JaVaPro";
+		UpperOrLower(str4);
+		
+		String str5 = "programming";
+		duplicate(str5);
+		
+		String str6 = "Java@123#";
+		mixed(str6);
+		
+		String str7 = "listen";
+		String str8 = "silent";
+		if(isAnagram(str7, str8)){
+			System.out.println("Anagram");	
+		}else {
+			System.out.println("not Anagram");
+		}
+		
+		nonReptd(str5);
+		removeDupli(str5);
+		freq(str5);
+		System.out.println(validAna(str7, str8));
+		
+		String stre= "loveleetcode";
+		System.out.println(firstUnique(stre)); 
+		
+
+	}
+}
